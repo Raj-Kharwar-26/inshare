0a2febf303a97724e4508aaf18ec87c2a6aad664
rajat jana <rajatjana2024@gmail.com>
2024-05-16 05:40:02 +0530

    3000 port is open only:

--- app.js
+++ app.js
@@ -37,5 +37,5 @@ app.get('/',(req,res)=>{
 
 
 //
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>console.log(`server started at PORT=${PORT}`));
